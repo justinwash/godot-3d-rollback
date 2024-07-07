f77271decaf90826a3779b1575bdd5fb273c12f3
Justin Wash <justin.wash@pm.me>
2024-07-07 14:51:48 -0500

    rapier changes

--- character.gd
+++ character.gd
@@ -7,14 +7,14 @@ const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta):
   # Add the gravity.
-  if not is_on_floor():
-    velocity += get_gravity() * delta
+  #if not is_on_floor():
+    #velocity += get_gravity() * delta
 
   # Handle jump.
   if Input.is_action_just_pressed("jump") and is_on_floor():
     velocity.y = JUMP_VELOCITY
 
-  if is_on_floor():
+  if true:
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
     var input_dir = Input.get_vector("left", "right", "up", "down")
